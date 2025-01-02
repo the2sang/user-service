5e64fb51094865bb7bcb0b6b3b4ec04feef692d9
the2sang <the2sang@gmail.com>
2025-01-02 17:47:55 +0900

    first commit

---
+++ client/user_client_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"testing"
+
+	users "github.com/the2sang/user-service/service"
+	"google.golang.org/grpc"
+  "google.golang.org/grpc/test/bufconn"
+)
+
+type dummyUserService struct {
+  users.UnimplementedUsersServer
+}
+
+func (s *dummyUserService) GetUser(
+  ctx context.Context,
+  in *users.UserGetRequest,
+) (*users.UserGetReply, error) {
+  u := users.User{
+    Id: "user-123-a",
+    FirstName: "jane",
+    LastName: "doe",
+    Age: 36,
+  }
+  return &users.UserGetReply{User: &u}, nil
+}
+
+func startTestGrpcServer() (*grpc.Server, *bufconn.Listener) {
+  l := bufconn.Listen(10)
+  s := grpc.NewServer()
+  users.RegisterUsersServer(s, &dummyUserService{})
+  go func() {
+    err := s.Serve(l)
+    if err != nil {
+      log.Fatal(err)
+    }
+  }()
+  return s, l
+}
+
+func TestGetUer(t *testing.T) {
+  s, l := startTestGrpcServer()
+  defer s.GracefulStop()
+
+  bufConnDialer := func(
+    ctx context.Context, addr string,
+  ) (net.Conn, error) {
+    return l.Dial()
+  }
+
+  conn, err := grpc.DialContext(
+    context.Background(),
+    "", grpc.WithInsecure(),
+    grpc.WithContextDialer(bufConnDialer),
+  )
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  c := getUserServiceClient(conn)
+  result, err := getUser(
+    c,
+    &users.UserGetRequest{Email: "jane@doe.com"},
+  )
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  if result.User.FirstName != "jane" || result.User.LastName != "doe" {
+    t.Fatalf(
+      "Expected: jane doe, Got: %s %s",
+      result.User.FirstName,
+      result.User.LastName,
+    )
+  }
+
+}
+
+
